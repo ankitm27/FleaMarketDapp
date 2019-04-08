c357806b85f7788325095cc74e433b40ce0c4895
alexytiger <yevseyevich@msn.com>
2019-04-08 12:37:56 -0400

    dig

--- ClientApp/src/app/core/components/dashboard/dashboard.component.ts
+++ ClientApp/src/app/core/components/dashboard/dashboard.component.ts
@@ -2,8 +2,30 @@ import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
+import { transition, style, animate, trigger } from '@angular/animations';
+
+export const DROP_BUDDY_ANIMATION = trigger('dropPoke', [
+  transition(':enter', [
+      style({ transform: 'translateY(-200px)', opacity: 0 }),
+      animate(
+          '750ms cubic-bezier(1.000, 0.000, 0.000, 1.000)',
+          style({ transform: 'translateY(0)', opacity: 1 })
+      ),
+  ]),
+  transition(':leave', [
+      style({ transform: 'translateY(0)', opacity: 1 }),
+      animate(
+          '200ms cubic-bezier(1.000, 0.000, 0.000, 1.000)',
+          style({ transform: 'translateY(-200px)', opacity: 0 })
+      ),
+  ]),
+]);
+
+
+
 @Component({
   selector: 'app-dashboard',
+  animations: [DROP_BUDDY_ANIMATION],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
