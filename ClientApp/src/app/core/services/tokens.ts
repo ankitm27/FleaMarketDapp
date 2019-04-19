7b51f2160e2232a768c33143ca854668787a3c4c
alexytiger <yevseyevich@msn.com>
2019-04-19 16:48:43 -0400

    dig

--- ClientApp/src/app/core/services/tokens.ts
+++ ClientApp/src/app/core/services/tokens.ts
@@ -1,16 +1,16 @@
 import { Injectable, InjectionToken, Inject } from '@angular/core';
 import { providers } from 'ethers';
 
-import ContractAbi from '../../../../../build/contracts/FleaMarket.json';
+// import ContractAbi from '../../../../../build/contracts/FleaMarket.json';
 
-export const MetaMaskWeb3Provider = new InjectionToken('MetaMask Web3 provider', {
+export const MetamaskWeb3Provider = new InjectionToken('Metamask Web3 provider', {
   providedIn: 'root',
   factory: () => (window as any).ethereum
 });
 
 @Injectable({ providedIn: 'root' })
-export class EtherProvider extends providers.Web3Provider {
-  constructor(@Inject(MetaMaskWeb3Provider) web3Provider) {
+export class EthersProvider extends providers.Web3Provider {
+  constructor(@Inject(MetamaskWeb3Provider) web3Provider) {
     super(web3Provider);
   }
 }
--- ClientApp/src/app/core/store/effects/web3-provider.effects.ts
+++ ClientApp/src/app/core/store/effects/web3-provider.effects.ts
@@ -5,8 +5,7 @@ import { Action } from '@ngrx/store';
 import { Observable, of, from, EMPTY as empty } from 'rxjs';
 import {exhaustMap, map, tap, catchError} from 'rxjs/operators';
 
-import { ethers } from 'ethers';
-import { MetaMaskWeb3Provider, EtherProvider } from '../../services/tokens';
+import { MetamaskWeb3Provider } from '../../services/tokens';
 import { Web3ProviderActions, SpinnerActions, ErrorActions}  from '../actions';
 
 
@@ -14,57 +13,56 @@ import { Web3ProviderActions, SpinnerActions, ErrorActions}  from '../actions';
 export class Web3ProviderEffects {
 
     constructor(
-      @Inject(MetaMaskWeb3Provider) private web3Provider,
-      @Inject(EtherProvider) private ethProvider,
+      @Inject(MetamaskWeb3Provider) private web3Provider,
       private readonly actions$: Actions<Web3ProviderActions.web3ProviderActionsUnion>) {
     }
 
     @Effect()
-      metaMaskEnable$: Observable<Action> = this.actions$.pipe(
-        ofType(Web3ProviderActions.init.type),
- 
-        exhaustMap(() => {
+    metaMaskEnable$: Observable<Action> = this.actions$.pipe(
+      ofType(Web3ProviderActions.init.type),
 
-          if ('enable' in this.web3Provider) {
+      exhaustMap(() => {
 
-            return from(this.web3Provider.enable())
-            .pipe(
+        if ('enable' in this.web3Provider) {
 
-              tap((ethAccounts: string[]) =>
-                console.log('Ethereum provider has been granted access to the following accounts', ethAccounts)
-              ),
-              map((ethAccounts: string[]) => {
+          return from(this.web3Provider.enable())
+          .pipe(
 
-                if (ethAccounts.length === 0) {
-                  return ErrorActions.errorMessage({errorMsg: 'Can not get any user accounts'});
-                }
+            tap((ethAccounts: string[]) =>
+              console.log('Ethereum provider has been granted access to the following accounts', ethAccounts)
+            ),
+            map((ethAccounts: string[]) => {
 
-                return  Web3ProviderActions.initSuccess();
-           
-              }),
+              if (ethAccounts.length === 0) {
+                return ErrorActions.errorMessage({errorMsg: 'Can not get any user accounts'});
+              }
 
-              // User denied account access
-              catchError((err: Error) => of(ErrorActions.errorMessage({errorMsg: err.message}))),
+              return  Web3ProviderActions.initSuccess();
+          
+            }),
 
-            ); 
+            // User denied account access
+            catchError((err: Error) => of(ErrorActions.errorMessage({errorMsg: err.message}))),
 
-          }
+          ); 
 
-          return empty;
+        }
 
-        })
+        return empty;
 
-      );
+      })
 
-      @Effect()
-        showSpinner$: Observable<Action> = this.actions$.pipe(
-          ofType(Web3ProviderActions.init.type),
-          map(() => SpinnerActions.show()));
+    );
 
-      @Effect()
-      hideSpinner$: Observable<Action> = this.actions$.pipe(
-        ofType(Web3ProviderActions.initSuccess.type, ErrorActions.errorMessage.type),
-        map(() => SpinnerActions.hide()));
+    @Effect()
+    showSpinner$: Observable<Action> = this.actions$.pipe(
+      ofType(Web3ProviderActions.init.type),
+      map(() => SpinnerActions.show()));
+
+    @Effect()
+    hideSpinner$: Observable<Action> = this.actions$.pipe(
+      ofType(Web3ProviderActions.initSuccess.type, ErrorActions.errorMessage.type),
+      map(() => SpinnerActions.hide()));
 
 
       // ''' to be continue ..put here effect to set address and balance
