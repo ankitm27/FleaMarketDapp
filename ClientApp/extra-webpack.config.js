05f1033be195d07ecdc3b7c1bc80c1cdd31f9181
alexytiger <yevseyevich@msn.com>
2019-04-28 18:18:27 -0400

    dig

---
+++ ClientApp/extra-webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  node: {
+    constants: true,
+    path: true,
+    os: true,
+    stream: true,
+    buffer: true
+  }
+};
--- ClientApp/src/app/core/services/tokens.ts
+++ ClientApp/src/app/core/services/tokens.ts
@@ -1,5 +1,6 @@
 import { Injectable, InjectionToken, Inject } from '@angular/core';
 import { providers } from 'ethers';
+// import { IpfsHttpClient } from 'ipfs-http-client';
 
 // import ContractAbi from '../../../../../build/contracts/FleaMarket.json';
 
@@ -14,3 +15,30 @@ export class Provider extends providers.Web3Provider {
     super(web3Provider);
   }
 }
+
+/*
+export const IPFS = new InjectionToken(
+  'The IPFS instance',
+  {
+    providedIn: 'root',
+    factory: () => new IpfsHttpClient('ipfs.infura.io', '5001', { protocol: 'https' })
+  },
+);
+*/
+
+/*
+export const WEB3 = new InjectionToken<Web3>('web3Token', {
+  providedIn: 'root',
+  factory: () => {
+    // based on https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+    try {
+      const provider = ('ethereum' in window) ? window['ethereum'] : Web3.givenProvider;
+
+      return new Web3(provider);
+    } catch (err) {
+      throw new Error('Unable to retrieve the injected Ethereum provider from  MetaMask');
+    }
+  }
+});
+*/
+
