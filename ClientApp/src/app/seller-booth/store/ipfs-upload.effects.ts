fdc36e5a53d79ff3666f8dca76305cd28a87ee3f
alexytiger <yevseyevich@msn.com>
2019-05-19 22:12:42 -0400

    dig

--- ClientApp/src/app/seller-booth/store/ipfs-upload.effects.ts
+++ ClientApp/src/app/seller-booth/store/ipfs-upload.effects.ts
@@ -1,9 +1,8 @@
 
-import { Injectable, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Actions, ofType, createEffect } from '@ngrx/effects';
 import { exhaustMap, map, tap, catchError } from 'rxjs/operators';
 import { of} from 'rxjs';
-import { Buffer } from 'buffer';
 
 import { IpfsDaemonService } from '../../core/services/ipfs-daemon.services';
 import * as IpfsUploadActions  from './ipfs-upload.actions';
@@ -22,7 +21,7 @@ export class IpfsUploadEffects {
       this.actions$.pipe(
         ofType(IpfsUploadActions.start),
         exhaustMap((data) => {
-          const fileStream =  Buffer.from(data.content);
+          const fileStream =  (window as any).IpfsHttpClient.Buffer.from(data.content);
           const path = data.path;
           
           return this.ipfsSrv.addFile(fileStream, path).pipe(
